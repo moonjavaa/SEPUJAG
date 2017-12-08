df848a039425aff22eaa4b8f6cbf52d7848cd341
sofzer <32210314+sofzer@users.noreply.github.com>
2017-12-08 17:04:59 +0100

    J'ai modifier la classe x

--- src/Principale.java
+++ src/Principale.java
@@ -16,7 +16,7 @@ public class Principale  {
 	}
 	public static void main(String[] args) throws IOException{
 		
-		Processus a= new Processus("p1",new Heure(12,30), new Duree(0,2) );
+		//Processus a= new Processus("p1",new Heure(12,30), new Duree(0,2) );
 		ArrayList Ensemble= new ArrayList();
 		
 		PrintWriter p= new PrintWriter("file1.txt");
@@ -32,14 +32,16 @@ public class Principale  {
 			String[] split=ss.split(" ");
 			String[] heuree=split[1].split(":");
 			String[] dureee=split[2].split(":");
-			Ensemble.add(new Processus(split[0],new Heure(Integer.parseInt(heuree[0]),Integer.parseInt(heuree[1])), new Duree(Integer.parseInt(dureee[0]),(int) (/*Integer.parseInt(dureee[1])*/Math.random()*10))));
+			Ensemble.add(new Processus(split[0],
+					new Heure(Integer.parseInt(heuree[0]),	Integer.parseInt(heuree[1])),
+				new Duree(Integer.parseInt(dureee[0]),(int) (/*Integer.parseInt(dureee[1])*/Math.random()*10))));
 		}
 		/*
 		for(int i=0; i<Ensemble.size();i++)
 			System.out.println(Ensemble.get(i));
 			
 		*/
-		Collections.sort(Ensemble,new Comparateur());
+		//Collections.sort(Ensemble,new Comparateur());
 		for(int i=0; i<Ensemble.size();i++)
 			System.out.println(Ensemble.get(i));
 		
