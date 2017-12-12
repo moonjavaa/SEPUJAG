ba4cc71db3db299da4be4ee73c6cb9c38718740f
sofzer <32210314+sofzer@users.noreply.github.com>
2017-12-12 09:54:35 +0100

    SJF presque pret

--- src/SJF.java
+++ src/SJF.java
@@ -5,12 +5,23 @@ import java.util.Collections;
 public class SJF {
 
 	public static void Sjf(ArrayList<Processus> Ensemble){
-
+		System.out.println("-----------------\nLecture a partir du fichier");
+		for(int i=0; i<Ensemble.size();i++)
+			System.out.println(Ensemble.get(i));
 		
 		Collections.sort(Ensemble,new ComparateurDureeEtTemps());
+		System.out.println("-----------------\nApres le tri");
 
 		for(int i=0; i<Ensemble.size();i++)
 			System.out.println(Ensemble.get(i));
+		
+		for(int i=1; i<Ensemble.size();i++){
+			Ensemble.get(i).tempArrive.setMinutes(Ensemble.get(i-1).tempArrive.getMinutes()+Ensemble.get(i-1).duree.getMinutes());
+		}
+		System.out.println("-----------------\n Sjf");
+		for(int i=0; i<Ensemble.size();i++)
+			System.out.println(Ensemble.get(i));
+
 		
 	}
 }
